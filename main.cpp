37ef438c27be973ffbf3f649d4f9f6200b3e222a
mhardki45-source <mhardki45@gmail.com>
2025-12-13 23:28:34 +0700

    debugging

--- main.cpp
+++ main.cpp
@@ -16,9 +16,7 @@ void initPapan() {
     }
 
 }
-  
-}
-void drawBoard() {
+void gambarPapan() {
     clear();
     mvprintw(1, 10, "=== TIC TAC TOE ===");
     mvprintw(2, 10, "Pemain: ");
@@ -30,11 +28,11 @@ void drawBoard() {
         int y = 4 + (i * 2);
 
         mvprintw(y, 10, "| ");
-        mvaddch(y, 12, board[i][0]);
+        mvaddch(y, 12, papan[i][0]);
         mvprintw(y, 13, " | ");
-        mvaddch(y, 16, board[i][1]);
+        mvaddch(y, 16, papan[i][1]);
         mvprintw(y, 17, " | ");
-        mvaddch(y, 20, board[i][2]);
+        mvaddch(y, 20, papan[i][2]);
         mvprintw(y, 21, " |");
 
         if (i < 2) {
@@ -48,6 +46,37 @@ void drawBoard() {
     refresh();
 }
 
+
+
+bool cekMenang(){
+    for (int i = 0; i < 3; i++) {
+        if (papan[i][0] == papan[i][1] && papan[i][1] == papan[i][2]) {
+            return true;
+        }
+    }
+    for (int j = 0; j < 3; j++) {
+        if (papan[0][j] == papan[1][j] && papan[1][j] == papan[2][j]) {
+            return true;
+        }
+    }
+    if (papan[0][0] == papan[1][1] && papan[1][1] == papan[2][2]) {
+        return true;
+    }
+    if (papan[0][2] == papan[1][1] && papan[1][1] == papan[2][0]) {
+        return true;
+    }
+    return false;
+}
+
+bool cekSeri() {
+    for (int y = 0; y < 3; y++) {
+        for (int x = 0; x < 3; x++) {
+            if (papan[y][x] != 'X' && papan[y][x] != 'O')
+                return false;
+        }
+    }
+    return true;
+}
 
 void pemain() {
     char input = getch();
@@ -96,44 +125,17 @@ void pemain() {
     }
 }
 
-bool cekMenang() {
-    for (int i = 0; i < 3; i++) {
-        if (papan[i][0] == papan[i][1] && papan[i][1] == papan[i][2]) {
-            return true;
-        }
-    }
-    for (int j = 0; j < 3; j++) {
-        if (papan[0][j] == papan[1][j] && papan[1][j] == papan[2][j]) {
-            return true;
-        }
-    }
-    if (papan[0][0] == papan[1][1] && papan[1][1] == papan[2][2]) {
-        return true;
-    }
-    if (board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
-        return true;
-    }
-    return false;
-}
-
-
-bool cekSeri() {
-
-    for (int y = 0; y < 3; y++) {
-        for (int x = 0; x < 3; x++) {
-            if (papan[y][x] == ' ')
-                return false;
-        }
-    }
-
-    return true;
-}
 int main() {
     initscr();
     noecho();
     cbreak();
     
     initPapan();
+    
+    while (gameAktif) {
+        gambarPapan();
+        pemain();
+    }
     
     endwin();
     return 0;
