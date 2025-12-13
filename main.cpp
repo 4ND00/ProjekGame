375f223c8f80f7fbfe50b6c8a7e4feecc4c45a4c
mhardki45-source <mhardki45@gmail.com>
2025-12-13 22:19:54 +0700

    gambar papan sama cek menang

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <ncurses.h>
+#include <iostream>
+using namespace std;
 
-void initPapan(){
-  char board[3][3];
+char papan[3][3];
+char pemainSekarang = 'X';
+bool gameAktif = true;
 
-void initBoard() {
+void initPapan() {
     char angka = '1';
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-            board[i][j] = angka;  // isi 1–9 untuk panduan posisi
+            papan[i][j] = angka;
             angka++;
         }
     }
@@ -15,18 +18,61 @@ void initBoard() {
 }
   
 }
-
-void gambarPapan(){
+void drawBoard() {
+    clear();
+    mvprintw(1, 10, "=== TIC TAC TOE ===");
+    mvprintw(2, 10, "Pemain: ");
+    mvaddch(2, 18, pemainSekarang);
   
+    mvprintw(3, 10, "+---+---+---+");
+
+    for (int i = 0; i < 3; i++) {
+        int y = 4 + (i * 2);
+
+        mvprintw(y, 10, "| ");
+        mvaddch(y, 12, board[i][0]);
+        mvprintw(y, 13, " | ");
+        mvaddch(y, 16, board[i][1]);
+        mvprintw(y, 17, " | ");
+        mvaddch(y, 20, board[i][2]);
+        mvprintw(y, 21, " |");
+
+        if (i < 2) {
+            mvprintw(y + 1, 10, "|---|---|---|");
+        }
+    }
+
+    mvprintw(9, 10, "+---+---+---+"); 
+    
+    mvprintw(11, 10, "Pilih posisi (1-9) atau 'q' untuk keluar");
+    refresh();
 }
+
 
 void pemain(){
   
 }
 
-void cekMenang(){
-  
+bool cekMenang() {
+    for (int i = 0; i < 3; i++) {
+        if (papan[i][0] == papan[i][1] && papan[i][1] == papan[i][2]) {
+            return true;
+        }
+    }
+    for (int j = 0; j < 3; j++) {
+        if (papan[0][j] == papan[1][j] && papan[1][j] == papan[2][j]) {
+            return true;
+        }
+    }
+    if (papan[0][0] == papan[1][1] && papan[1][1] == papan[2][2]) {
+        return true;
+    }
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+        return true;
+    }
+    return false;
 }
+
 
 bool cekSeri() {
 
@@ -39,20 +85,14 @@ bool cekSeri() {
 
     return true;
 }
-
-    return true;
-}
-  
-}
-
 int main() {
-    initscr();   
+    initscr();
     noecho();
-
-    initBoard();  
-    drawBoard();  
-
-    getch();      
-    endwin();     
-    return 0;
+    cbreak();
+    
+    initPapan();
+    
+    endwin();
+    return 0;
 }
+
